7731464cffeab12fc07f418684cc8a98350184ff
Andreas Wassum <a.wassum@gmail.com>
2023-07-04 19:27:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import json
+
+from fastapi import FastAPI
+
+import random
+import yfinance as yf
+
+app = FastAPI(version="0.0.1")
+
+
+@app.get("/get-random-number")
+async def root():
+    return {"example": "This is the example text", "number": 0}
+
+
+@app.get("/current-price/{symbol}")
+async def getCurrentPrice(symbol: str):
+    ticker = yf.Ticker(symbol)
+    history = ticker.info
+    return json.dumps(history)
