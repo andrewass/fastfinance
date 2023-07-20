8410a42c2b17d11b697dd11d5a8496ab6ee310bd
Andreas Wassum <a.wassum@gmail.com>
2023-07-20 20:52:45 +0200

    Grouped into routers

--- app/main.py
+++ app/main.py
@@ -1,38 +1,7 @@
-import json
-from dataclasses import dataclass
-
-import yfinance as yf
 from fastapi import FastAPI
+from .routers import price
 
 app = FastAPI(version="0.0.1")
 
+app.include_router(price.router)
 
-@dataclass
-class SymbolsRequest:
-    symbols: list[str]
-
-
-@dataclass
-class CurrentPrice:
-    symbol: str
-    currentPrice: float
-    previousClose: float
-    currency: str
-
-
-@app.get("/current-price-symbol/{symbol}")
-async def get_current_price_symbol(symbol: str):
-    return get_current_price(symbol)
-
-
-@app.post("/current-price-symbols")
-async def get_current_price_symbols(symbols: SymbolsRequest):
-    return list(map(get_current_price, symbols.symbols))
-
-
-def get_current_price(symbol: str):
-    ticker = yf.Ticker(symbol)
-    current_price = ticker.info.get("currentPrice")
-    previous_close = ticker.info.get("previousClose")
-    currency = ticker.info.get("currency")
-    return CurrentPrice(symbol, current_price, previous_close, currency)
